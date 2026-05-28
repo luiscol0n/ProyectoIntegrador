25e6113e1696ac6dd8fd3740548ebe02319a2bb0
trian <trian@MSI>
2026-05-27 23:47:23 -0400

    Insercion de lecturas

--- ProyectoIntegrador/src/database/SQLData.java
+++ ProyectoIntegrador/src/database/SQLData.java
@@ -3,10 +3,11 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 public class SQLData {
     // Cambia "IP_DE_TU_SERVIDOR" por la IP real de tu Ubuntu Live Server
-    private static final String URL = "jdbc:mariadb://192.168.34.131:3306/EstacionMYeteorologica";
+    private static final String URL = "jdbc:mariadb://192.168.34.191:3306/EstacionMeteorologica";
     private static final String USER = "grupo1s";
     private static final String PASS = "g1s";
 
@@ -18,5 +19,44 @@ public class SQLData {
             System.err.println("[-] No se encontró el Driver de MariaDB: " + e.getMessage());
         }
         return DriverManager.getConnection(URL, USER, PASS);
+    }
+    public static void insertarLectura(
+
+            int estacionId,
+            double temperatura,
+            double humedad
+
+    ) {
+
+        try {
+
+            Connection conn = getConexion();
+
+            String sql =
+                    "INSERT INTO lecturas " +
+                    "(id_estacion, temperatura, humedad) " +
+                    "VALUES (?, ?, ?)";
+
+            PreparedStatement stmt =
+                    conn.prepareStatement(sql);
+
+            stmt.setInt(1, estacionId);
+
+            stmt.setDouble(2, temperatura);
+
+            stmt.setDouble(3, humedad);
+
+            stmt.executeUpdate();
+
+            System.out.println(
+                    "Lectura guardada"
+            );
+
+            conn.close();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
     }
 }
